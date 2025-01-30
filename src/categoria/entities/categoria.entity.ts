14002d75470c6fb2e5c0cdd4ef025b8c170d6a0c
Ana Paula <anapaula-sds@hotmail.com>
2025-01-30 12:05:59 -0300

    :sparkles: Inclusão classe Categoria

---
+++ src/categoria/entities/categoria.entity.ts
@@ -0,0 +1,17 @@
+import { Transform, TransformFnParams } from "class-transformer"
+import { IsNotEmpty } from "class-validator"
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity({ name: 'tb_categoria' })
+export class Categoria {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Transform(({ value }: TransformFnParams) => value?.trim())
+    @IsNotEmpty()
+    @Column({length: 1000, nullable: false})
+    nome: string
+
+    //Relacionamento abaixo
+}
