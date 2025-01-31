f6598448b3c305d91b2f4f829eac9b4552b9f744
Carlos Henrique Nunes <carloshnunes383@gmail.com>
2025-01-31 13:14:54 -0300

    :sparkles: ApiProperty, ApiTags e ApiBearerAuth - Categoria

--- src/categoria/entities/categoria.entity.ts
+++ src/categoria/entities/categoria.entity.ts
@@ -2,18 +2,22 @@ import { Transform, TransformFnParams } from "class-transformer"
 import { IsNotEmpty } from "class-validator"
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
 import { Produto } from "../../produto/entities/produto.entity"
+import { ApiProperty } from "@nestjs/swagger"
 
 @Entity({ name: 'tb_categoria' })
 export class Categoria {
 
     @PrimaryGeneratedColumn()
+    @ApiProperty()
     id: number
 
     @Transform(({ value }: TransformFnParams) => value?.trim())
     @IsNotEmpty()
     @Column({length: 1000, nullable: false})
+    @ApiProperty()
     nome: string
 
+    @ApiProperty()
     @OneToMany(() => Produto, (produto) => produto.categoria)
     produto: Produto[];
 }
